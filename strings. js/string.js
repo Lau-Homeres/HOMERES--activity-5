2935dfbd21d1b844ad89c47160f23aed48f72df1
Lau Homeres <laurencehomeres0@gmail.com>
2024-09-15 12:32:16 +0800

    initial commit

---
+++ strings. js/string.js
@@ -0,0 +1,17 @@
+let favActorFirstName = "Joshua";
+let favActorLastName = "Garcia";
+let fullName = favActorFirstName + " " + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+let lowercase = "josh".toUpperCase();
+
+let message = "My favorite actor is" + " " + fullName.toLocaleUpperCase();
+
+message += ", his best movie is UN/HAPPY.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
